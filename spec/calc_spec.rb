589ee61c4413be148decce8ae266c71d7b01c653
Zdenko Nevrala <nevralaz@gmail.com>
2017-10-09 21:19:29 +0200

    Fix spec for new audience name.

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -124,7 +124,7 @@ describe 'Calculations' do
 
     context "with female || (red && old) audience" do
       let(:female_or_red_old_audience) { } # TODO: create an audience
-      let(:audience) { female_or_red_and_old_audience }
+      let(:audience) { female_or_red_old_audience }
 
       # TODO: make test
     end
